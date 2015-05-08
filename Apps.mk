2e42394a2229ba48a0f1b50bda4902526473b1e0
Axel Fagerstedt <axel.fagerstedt@intel.com>
2015-05-08 01:26:37 -0700

    rename local modules to not conflict with boringssl, we still need openssl for sbsigntool adn image signing
    Change-Id: I8b061b114b10b28d3485d3130a751351caabb41c

--- Apps.mk
+++ Apps.mk
@@ -9,7 +9,7 @@ LOCAL_MODULE_STEM_32 := openssl
 LOCAL_MODULE_STEM_64 := openssl64
 LOCAL_CLANG := true
 LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libssl libcrypto
+LOCAL_SHARED_LIBRARIES := libssl2 libcrypto2
 include $(LOCAL_PATH)/Apps-config-target.mk
 include $(LOCAL_PATH)/android-config.mk
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Apps.mk
@@ -18,7 +18,7 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_MODULE := openssl
 LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libssl-host libcrypto-host
+LOCAL_SHARED_LIBRARIES := libssl-host2 libcrypto-host2
 include $(LOCAL_PATH)/Apps-config-host.mk
 include $(LOCAL_PATH)/android-config.mk
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Apps.mk
