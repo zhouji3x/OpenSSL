7cd5656537bad6c5d01e5be3bb8dec768b8a36b3
Jeremy Compostella <jeremy.compostella@intel.com>
2015-10-02 18:08:41 +0200

    support openssl host compilation
    Change-Id: I2c16ade250369b9181f548e7abecdc2c14542d37
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-4013
    Signed-off-by: Jeremy Compostella <jeremy.compostella@intel.com>
    Reviewed-on: https://android.intel.com:443/419393

--- Apps.mk
+++ Apps.mk
@@ -4,21 +4,18 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := openssl
-LOCAL_MULTILIB := both
-LOCAL_MODULE_STEM_32 := openssl
-LOCAL_MODULE_STEM_64 := openssl64
-LOCAL_CLANG := true
-LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libssl2 libcrypto2
-include $(LOCAL_PATH)/Apps-config-target.mk
-include $(LOCAL_PATH)/android-config.mk
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Apps.mk
-include $(BUILD_EXECUTABLE)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := openssl
+ifeq ($(HOST_OS),windows)
+  LOCAL_LDLIBS += -lws2_32 -lgdi32
+endif
+
+ifeq ($(HOST_OS),linux)
+  LOCAL_LDLIBS += -lrt -ldl -lpthread
+  LOCAL_CXX_STL := libc++_static
+endif
+
 LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libssl-host2 libcrypto-host2
+LOCAL_STATIC_LIBRARIES := libssl-static2 libcrypto_static2
 include $(LOCAL_PATH)/Apps-config-host.mk
 include $(LOCAL_PATH)/android-config.mk
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/android-config.mk $(LOCAL_PATH)/Apps.mk
