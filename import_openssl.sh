895cfaa3a64915124f9a255e36879f7c645ac876
Leo Sartre <leo.sartre@intel.com>
2016-04-26 15:16:37 +0200

    Prepare for update to openssl 1.0.2h
    Cleaning up the import_openssl.sh script to keep only x86 related.
    Remove legacy patches, introduce new one to build app without ts.
    Fix openssl.config and import_openssl.sh to make the import ok.
    Remove all non x86 arch.
    Remove trusty stuff.
    
    Change-Id: I59a52ed9fe11f0f18bf303d9cc2fe708bf1ceaf3
    Signed-off-by: Leo Sartre <leo.sartre@intel.com>
    Tracked-On: OAM-17738
    Signed-off-by: marie cecile afantenos <marie.cecile.afantenos@intel.com>
    Reviewed-on: 506076

--- import_openssl.sh
+++ import_openssl.sh
@@ -50,8 +50,6 @@ function usage() {
   fi
   echo "Usage:"
   echo "  ./import_openssl.sh import </path/to/openssl-*.tar.gz>"
-  echo "  ./import_openssl.sh regenerate <patch/*.patch>"
-  echo "  ./import_openssl.sh generate <patch/*.patch> </path/to/openssl-*.tar.gz>"
   exit 1
 }
 
@@ -82,25 +80,13 @@ function main() {
   fi
 
   declare -r command=$1
-  shift || usage "No command specified. Try import, regenerate, or generate."
+  shift || usage "No command specified. Try import."
   if [ "$command" = "import" ]; then
     declare -r tar=$1
     shift || usage "No tar file specified."
     import $tar
-  elif [ "$command" = "regenerate" ]; then
-    declare -r patch=$1
-    shift || usage "No patch file specified."
-    [ -d $OPENSSL_DIR ] || usage "$OPENSSL_DIR not found, did you mean to use generate?"
-    [ -d $OPENSSL_DIR_ORIG ] || usage "$OPENSSL_DIR_ORIG not found, did you mean to use generate?"
-    regenerate $patch
-  elif [ "$command" = "generate" ]; then
-    declare -r patch=$1
-    shift || usage "No patch file specified."
-    declare -r tar=$1
-    shift || usage "No tar file specified."
-    generate $patch $tar
   else
-    usage "Unknown command specified $command. Try import, regenerate, or generate."
+    usage "Unknown command specified $command. Try import."
   fi
 }
 
@@ -120,33 +106,6 @@ function default_asm_file () {
   else
     echo "${1%%.pl}.S"
   fi
-}
-
-# Generate an ARM assembly file.
-# $1: generator (perl script)
-# $2: [optional] output file name
-function gen_asm_arm () {
-  local OUT
-  OUT=$(default_asm_file "$@")
-  $PERL_EXE "$1" void "$OUT" > "$OUT"
-}
-
-# Generate an ARMv8 64-bit assembly file.
-# $1: generator (perl script)
-# $2: [optional] output file name
-function gen_asm_arm64 () {
-  local OUT
-  OUT=$(default_asm_file "$@")
-  $PERL_EXE "$1" linux64 "$OUT" > "$OUT"
-}
-
-function gen_asm_mips () {
-  local OUT
-  OUT=$(default_asm_file "$@")
-  # The perl scripts expect to run the target compiler as $CC to determine
-  # the endianess of the target. Setting CC to true is a hack that forces the scripts
-  # to generate little endian output
-  CC=true $PERL_EXE "$1" o32 > "$OUT"
 }
 
 function gen_asm_x86 () {
@@ -218,7 +177,7 @@ function check_asm_flags() {
   local defines="OPENSSL_CRYPTO_DEFINES_$arch"
 
   PERL=/usr/bin/perl run_verbose ./Configure $CONFIGURE_ARGS $target
-
+ 
   unsorted_flags="$(awk '/^CFLAG=/ { sub(/^CFLAG= .*-Wall /, ""); gsub(/-D/, ""); print; }' Makefile)"
   unsorted_flags="$unsorted_flags $(scan_opensslconf_for_flags "${CRYPTO_CONF_FLAGS[@]}")"
 
@@ -235,7 +194,7 @@ function check_asm_flags() {
 }
 
 # Run Configure and generate headers
-# $1: 32 for 32-bit arch, 64 for 64-bit arch, trusty for Trusty
+# $1: 32 for 32-bit arch, 64 for 64-bit arch
 # $2: 1 if building for static version
 # Out: returns the cflags and depflags in variable $flags
 function generate_build_config_headers() {
@@ -248,11 +207,7 @@ function generate_build_config_headers() {
       outname="static-$1"
   fi
 
-  if [[ $1 == trusty ]] ; then
-    PERL=/usr/bin/perl run_verbose ./Configure $CONFIGURE_ARGS_TRUSTY
-  else
-    PERL=/usr/bin/perl run_verbose ./Configure $CONFIGURE_ARGS ${!configure_args_bits} ${!configure_args_stat}
-  fi
+  PERL=/usr/bin/perl run_verbose ./Configure $CONFIGURE_ARGS ${!configure_args_bits} ${!configure_args_stat}
 
   rm -f apps/CA.pl.bak crypto/opensslconf.h.bak
   mv -f crypto/opensslconf.h crypto/opensslconf-$outname.h
@@ -271,7 +226,7 @@ function generate_build_config_headers() {
 # Run Configure and generate makefiles
 function generate_build_config_mk() {
   chmod +x ./Configure
-  for bits in 32 64 trusty; do
+  for bits in 32 64; do
     # Header flags are output in $flags, first static, then dynamic
     generate_build_config_headers $bits 1
     local flags_static=$flags
@@ -297,14 +252,10 @@ function generate_opensslconf_h() {
   echo "Generating opensslconf.h"
   (
   echo "// Auto-generated - DO NOT EDIT!"
-  echo "#ifndef OPENSSL_SYS_TRUSTY"
   echo "#if defined(__LP64__)"
   echo "#include \"opensslconf-64.h\""
   echo "#else"
   echo "#include \"opensslconf-32.h\""
-  echo "#endif"
-  echo "#else"
-  echo "#include \"opensslconf-trusty.h\""
   echo "#endif"
   ) > crypto/opensslconf.h
   # Generate a compatible version for the static library builds
@@ -409,7 +360,7 @@ print_defines_in_mk() {
 function generate_config_mk() {
   declare -r output="$1"
   declare -r prefix="$2"
-  declare -r all_archs="arm arm64 x86 x86_64 mips"
+  declare -r all_archs="x86 x86_64"
 
   echo "Generating $(basename $output)"
   (
@@ -436,7 +387,7 @@ LOCAL_ADDITIONAL_DEPENDENCIES += \$(LOCAL_PATH)/$(basename $output)
     print_vardef_in_mk common_src_files $common_sources
 
     common_includes=$(var_sorted_value OPENSSL_${prefix}_INCLUDES)
-    print_vardef_with_prefix_in_mk common_c_includes external/openssl/ $common_includes
+    print_vardef_with_prefix_in_mk common_c_includes vendor/intel/external/openssl/ $common_includes
 
     for arch in $all_archs; do
       arch_defines=$(var_sorted_value OPENSSL_${prefix}_DEFINES_${arch})
@@ -492,8 +443,6 @@ function import() {
   cd $OPENSSL_DIR
 
   # Check the ASM flags for each arch
-  check_asm_flags arm linux-armv4
-  check_asm_flags arm64 linux-aarch64
   check_asm_flags x86 linux-elf
   check_asm_flags x86_64 linux-x86_64
 
@@ -511,32 +460,6 @@ function import() {
       cp include/openssl/$target $i
     fi
   done
-
-  # Generate arm asm
-  gen_asm_arm crypto/aes/asm/aes-armv4.pl
-  gen_asm_arm crypto/aes/asm/aesv8-armx.pl
-  gen_asm_arm crypto/aes/asm/bsaes-armv7.pl
-  gen_asm_arm crypto/bn/asm/armv4-gf2m.pl
-  gen_asm_arm crypto/bn/asm/armv4-mont.pl
-  gen_asm_arm crypto/modes/asm/ghash-armv4.pl
-  gen_asm_arm crypto/modes/asm/ghashv8-armx.pl
-  gen_asm_arm crypto/sha/asm/sha1-armv4-large.pl
-  gen_asm_arm crypto/sha/asm/sha256-armv4.pl
-  gen_asm_arm crypto/sha/asm/sha512-armv4.pl
-
-  # Generate armv8 asm
-  gen_asm_arm64 crypto/aes/asm/aesv8-armx.pl crypto/aes/asm/aesv8-armx-64.S
-  gen_asm_arm64 crypto/modes/asm/ghashv8-armx.pl crypto/modes/asm/ghashv8-armx-64.S
-  gen_asm_arm64 crypto/sha/asm/sha1-armv8.pl
-  gen_asm_arm64 crypto/sha/asm/sha512-armv8.pl crypto/sha/asm/sha256-armv8.S
-  gen_asm_arm64 crypto/sha/asm/sha512-armv8.pl
-
-  # Generate mips asm
-  gen_asm_mips crypto/aes/asm/aes-mips.pl
-  gen_asm_mips crypto/bn/asm/mips.pl crypto/bn/asm/bn-mips.S
-  gen_asm_mips crypto/bn/asm/mips-mont.pl
-  gen_asm_mips crypto/sha/asm/sha1-mips.pl
-  gen_asm_mips crypto/sha/asm/sha512-mips.pl crypto/sha/asm/sha256-mips.S
 
   # Generate x86 asm
   gen_asm_x86 crypto/x86cpuid.pl
@@ -557,24 +480,31 @@ function import() {
   gen_asm_x86 crypto/bf/asm/bf-586.pl
 
   # Generate x86_64 asm
-  gen_asm_x86_64 crypto/x86_64cpuid.pl
-  gen_asm_x86_64 crypto/sha/asm/sha1-x86_64.pl
-  gen_asm_x86_64 crypto/sha/asm/sha512-x86_64.pl crypto/sha/asm/sha256-x86_64.S
-  gen_asm_x86_64 crypto/sha/asm/sha512-x86_64.pl
-  gen_asm_x86_64 crypto/modes/asm/ghash-x86_64.pl
-  gen_asm_x86_64 crypto/aes/asm/aesni-x86_64.pl
-  gen_asm_x86_64 crypto/aes/asm/vpaes-x86_64.pl
-  gen_asm_x86_64 crypto/aes/asm/bsaes-x86_64.pl
   gen_asm_x86_64 crypto/aes/asm/aes-x86_64.pl
+  gen_asm_x86_64 crypto/aes/asm/aesni-mb-x86_64.pl
   gen_asm_x86_64 crypto/aes/asm/aesni-sha1-x86_64.pl
-  gen_asm_x86_64 crypto/md5/asm/md5-x86_64.pl
-  gen_asm_x86_64 crypto/bn/asm/modexp512-x86_64.pl
-  gen_asm_x86_64 crypto/bn/asm/x86_64-mont.pl
+  gen_asm_x86_64 crypto/aes/asm/aesni-sha256-x86_64.pl
+  gen_asm_x86_64 crypto/aes/asm/aesni-x86_64.pl
+  gen_asm_x86_64 crypto/aes/asm/bsaes-x86_64.pl
+  gen_asm_x86_64 crypto/aes/asm/vpaes-x86_64.pl
+  gen_asm_x86_64 crypto/bn/asm/rsaz-avx2.pl
+  gen_asm_x86_64 crypto/bn/asm/rsaz-x86_64.pl
   gen_asm_x86_64 crypto/bn/asm/x86_64-gf2m.pl
+  gen_asm_x86_64 crypto/bn/asm/x86_64-mont.pl
   gen_asm_x86_64 crypto/bn/asm/x86_64-mont5.pl
-  gen_asm_x86_64 crypto/rc4/asm/rc4-x86_64.pl
+  gen_asm_x86_64 crypto/ec/asm/ecp_nistz256-x86_64.pl
+  gen_asm_x86_64 crypto/md5/asm/md5-x86_64.pl
+  gen_asm_x86_64 crypto/modes/asm/aesni-gcm-x86_64.pl
+  gen_asm_x86_64 crypto/modes/asm/ghash-x86_64.pl
   gen_asm_x86_64 crypto/rc4/asm/rc4-md5-x86_64.pl
-
+  gen_asm_x86_64 crypto/rc4/asm/rc4-x86_64.pl
+  gen_asm_x86_64 crypto/sha/asm/sha1-mb-x86_64.pl
+  gen_asm_x86_64 crypto/sha/asm/sha1-x86_64.pl
+  gen_asm_x86_64 crypto/sha/asm/sha256-mb-x86_64.pl
+  gen_asm_x86_64 crypto/sha/asm/sha512-x86_64.pl
+  gen_asm_x86_64 crypto/sha/asm/sha512-x86_64.pl crypto/sha/asm/sha256-x86_64.S
+  gen_asm_x86_64 crypto/x86_64cpuid.pl
+  
   # Setup android.testssl directory
   mkdir android.testssl
   cat test/testssl | \
@@ -595,7 +525,6 @@ function import() {
 
   generate_config_mk Crypto-config-target.mk CRYPTO target
   generate_config_mk Crypto-config-host.mk CRYPTO host
-  generate_config_mk Crypto-config-trusty.mk CRYPTO_TRUSTY target
   generate_config_mk Ssl-config-target.mk SSL target
   generate_config_mk Ssl-config-host.mk SSL host
   generate_config_mk Apps-config-target.mk APPS target
@@ -611,30 +540,6 @@ function import() {
     mv $OPENSSL_DIR/$i .
   done
 
-  cleantar
-}
-
-function regenerate() {
-  declare -r patch=$1
-
-  generatepatch $patch
-}
-
-function generate() {
-  declare -r patch=$1
-  declare -r OPENSSL_SOURCE=$2
-
-  untar $OPENSSL_SOURCE
-  applypatches $OPENSSL_DIR_ORIG $patch
-  prune
-
-  for i in $NEEDED_SOURCES; do
-    echo "Restoring $i"
-    rm -r $OPENSSL_DIR/$i
-    cp -rf $i $OPENSSL_DIR/$i
-  done
-
-  generatepatch $patch
   cleantar
 }
 
@@ -688,12 +593,15 @@ function applypatches () {
 
   cd $dir
 
+  git init .
+  git add .
+  git commit -s -m "Initial commit" >/dev/null
   # Apply appropriate patches
   patches=(../patches/[0-9][0-9][0-9][0-9]-*.patch)
   for i in "${patches[@]}"; do
     if [[ $skip_patch != ${i##*/} ]]; then
       echo "Applying patch $i"
-      patch -p1 < $i || die "Could not apply $i. Fix source and run: $0 regenerate patches/${i##*/}"
+      git am $i
     else
       echo "Skiping patch ${i##*/}"
     fi
@@ -704,25 +612,6 @@ function applypatches () {
   find . \( -type f -o -type l \) -name "*.orig" -print0 | xargs -0 rm -f
 
   cd ..
-}
-
-function generatepatch() {
-  declare -r patch=$1
-
-  # Cleanup stray files before generating patch
-  find $OPENSSL_DIR -type f -name "*.orig" -print0 | xargs -0 rm -f
-  find $OPENSSL_DIR -type f -name "*~" -print0 | xargs -0 rm -f
-
-  # Find the files the patch touches and only keep those in the output patch
-  declare -r sources=`patch -p1 --dry-run -d $OPENSSL_DIR < $patch  | awk '/^patching file / { print $3 }'`
-
-  rm -f $patch
-  touch $patch
-  for i in $sources; do
-    LC_ALL=C TZ=UTC0 diff -aup $OPENSSL_DIR_ORIG/$i $OPENSSL_DIR/$i >> $patch && die "ERROR: No diff for patch $path in file $i"
-  done
-  echo "Generated patch $patch"
-  echo "NOTE To make sure there are not unwanted changes from conflicting patches, be sure to review the generated patch."
 }
 
 main $@
